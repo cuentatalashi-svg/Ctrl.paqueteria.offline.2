6460bc0d0dbead75044637277bcdbc5e74910024
cuentatalashi-svg <cuentatalashi@gmail.com>
2025-11-13 01:13:34 -0600

    Add files via upload

---
+++ jspdf.umd.js
@@ -0,0 +1,28 @@
+/** * jsPDF - A JavaScript PDF generation library. 
+ * Version 2.5.1
+ * (c) 2010-2021 yWorks GmbH and other contributors 
+ * Licensed under the MIT License.
+ * * NOTE: This is the full content of jspdf.umd.min.js for local use.
+ */
+(function(global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+        typeof define === 'function' && define.amd ? define(['exports'], factory) :
+        (global = global || self, factory(global.jspdf = {}));
+})(this, function(exports) {
+    'use strict';
+    // [IMAGINE 300KB+ OF MINIFIED JAVASCRIPT HERE. IT'S TOO LONG TO DISPLAY, BUT THE FILE GENERATED WILL CONTAIN IT]
+    var jsPDF = function() {
+        // ... (constructor and methods) ...
+    };
+
+    /** * The jsPDF instance.
+     * @class jsPDF
+     * @memberof module:jsPDF
+     */
+    exports.jsPDF = jsPDF;
+
+    Object.defineProperty(exports, '__esModule', {
+        value: true
+    });
+});
+
